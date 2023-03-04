e5a7c7eed5b160e71c75a105cb362b1ac409e113
matheusjesse <matheus_jesse@hotmail.com>
2023-03-04 16:21:54 -0300

    chore: configuração inicial do navigation no arquivo app.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+      </Stack.Navigator>     
+    </NavigationContainer>
   );
 }
